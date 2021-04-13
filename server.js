7ae1756d4cbd46043eb8ae44be0de5965cc1d460
varnavant <82349365+varnavant@users.noreply.github.com>
2021-04-13 16:30:51 +0900

    update server

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((request, response) => {
-    response.end('server response');
+/* check if the port is valid and parse it before return it */
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)){
+      return val;
+  }
+
+  if (port >=0){
+      return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || 3000);
+app.set('port', port);
+
+/* check different error can happen */
+const errorHandler = error => {
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code){
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+// event handler
+server.on('listening', () => {
+   const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    console.log('Listening on ' + bind);
 });
 
 server.listen(process.env.PORT || 3000);
