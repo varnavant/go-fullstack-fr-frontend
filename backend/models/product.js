447adb07ffe795844ac02810904b85634c5494cb
varnavant <82349365+varnavant@users.noreply.github.com>
2021-04-19 12:20:36 +0900

    update

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema({
-    title: { type: String, required: true },
+const productSchema = mongoose.Schema({
+    name: { type: String, required: true },
     description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    userId: { type: String, required: true },
     price: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
 });
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model('Product', productSchema);
