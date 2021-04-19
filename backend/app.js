9646eeb0d68d7027e49d4fef1482409b063d8d3c
varnavant <82349365+varnavant@users.noreply.github.com>
2021-04-19 11:01:21 +0900

    update api

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
     delete req.body._id; // delete fake id create by frontend
@@ -34,16 +35,28 @@ app.post('/api/stuff', (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id :  req.params.id})
+        .then(() => res.status(200).json({ message: 'deleted!'}))
+        .catch(error => res.status(400).json({ error}));
 });
 
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
         .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
